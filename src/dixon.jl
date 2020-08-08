eaae3cbd5c9030169dbc83c3313dd945e884ef05
kalmarek <kalmar@amu.edu.pl>
2020-08-08 22:07:35 +0200

    add Dixon algorithm over GF{p}

--- src/dixon.jl
+++ src/dixon.jl
@@ -9,10 +9,34 @@ function dixon_prime(cclasses::AbstractVector)
 end
 
 function dixon_prime(ordG::Integer, exponent::Integer)
-    p = 2floor(Int, sqrt(ordG))
+    p = 2 * floor(Int, sqrt(ordG))
     while true
-        p = nextprime(p+1)
+        p = nextprime(p + 1)
         isone(p % exponent) && break # we need -1 to be in the field
     end
     return p
+end
+
+function common_esd(Ns, F::Type{<:FiniteFields.GF})
+    @assert !isempty(Ns)
+    esd = EigenSpaceDecomposition(F.(first(Ns)))
+    for N in Iterators.rest(Ns, 2)
+        esd = refine(esd, F.(N))
+        @debug N esd.eigspace_ptrs
+        isdiag(esd) && return esd
+    end
+    return esd
+end
+
+function characters_dixon(
+    cclasses::AbstractVector{<:AbstractOrbit},
+    F::Type{<:FiniteFields.GF},
+)
+    Ns = (CCMatrix(cclasses, i) for i = 1:length(cclasses))
+    esd = common_esd(Ns, F)
+    inv_ccls = _inv_of(cclasses)
+    return [
+        normalize!(Character(vec(eigensubspace), inv_ccls, cclasses))
+        for eigensubspace in esd
+    ]
 end
