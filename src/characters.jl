cf0ed51739d46b01b619f60174356b4fb047eea7
kalmarek <kalmar@amu.edu.pl>
2020-08-08 21:53:18 +0200

    separate normalization of Character from its definition

--- src/characters.jl
+++ src/characters.jl
@@ -6,22 +6,16 @@ function LinearAlgebra.dot(
     χ::AbstractClassFunction{T},
     ψ::AbstractClassFunction{T},
 ) where {T}
-    # TODO: @assert v.cc == w.cc
 
-    R = parent(χ[1]) # TODO make something better here
-    val = zero(R)
+    val = sum(length(cc) * χ[i] * ψ[-i] for (i, cc) in enumerate(conjugacy_classes(χ)))
 
-    for (i, cc) in enumerate(classes(χ))
-        val += R(length(cc)) * χ[i] * ψ[-i]
-    end
-
-    orderG = R(sum(length, classes(χ)))
-    val *= inv(orderG)
+    orderG = sum(length, conjugacy_classes(χ))
+    val = div(val,orderG)
     return val
 end
 
-function (χ::AbstractClassFunction)(g::GroupElem)
-    for (i, cc) in enumerate(classes(χ))
+function (χ::AbstractClassFunction)(g::PermutationGroups.AbstractPerm)
+    for (i, cc) in enumerate(conjugacy_classes(χ))
         g ∈ cc && return χ[i]
     end
     throw(DomainError(g, "element does not belong to conjugacy classes of χ"))
@@ -30,48 +24,48 @@ end
 ####################################
 # Characters
 
-mutable struct Character{T,CCl} <: AbstractClassFunction{T}
+mutable struct Character{T,CCl<:AbstractOrbit} <: AbstractClassFunction{T}
     vals::Vector{T}
     inv_of::Vector{Int}
     cc::Vector{CCl}
-
-    function Character(
-        v::AbstractVector{T},
-        ccls::AbstractVector{CCl},
-        inv_of = _inv_of(ccls),
-    ) where {T,CCl}
-
-        χ = new{T,CCl}(v, inv_of, ccls)
-
-        # initial normalization
-        R = parent(first(v))
-        id = one(first(first(ccls)))
-        if !isone(χ(id))
-            χ.vals .*= inv(χ(id))
-        end
-
-        # computing the degree of χ:
-        deg_χ = sqrt(inv(dot(χ, χ)))
-        @debug χ.vals, deg_χ
-
-        # renormalizing χ
-        χ.vals .*= deg_χ
-
-        return χ
-    end
 end
 
-function _inv_of(cc::AbstractVector)
+function Character(
+    v::AbstractVector{T},
+    ccls::AbstractVector{CCl},
+) where {T,CCl<:AbstractOrbit}
+    χ = Character{T,CCl}(v, _inv_of(ccls), ccls)
+    return χ
+end
+
+function _inv_of(cc::AbstractVector{<:AbstractOrbit})
     inv_of = zeros(Int, size(cc))
     for (i, c) in enumerate(cc)
         g = inv(first(c))
         inv_of[i] = something(findfirst(k -> g in k, cc), 0)
     end
-    @assert !any(iszero, inv_of) "Could not find the conjugacy class of $g."
+    any(iszero, inv_of) &&
+        throw(ArgumentError("Could not find the conjugacy class for inverse of $(first(cc[findfirst(iszero, inv_of)]))."))
     return inv_of
 end
 
-classes(χ::Character) = χ.cc
+function normalize!(χ::Character)
+
+    id = one(first(first(conjugacy_classes(χ))))
+    k = χ(id)
+
+    # computing the degree of χ:
+    n = sqrt(inv(dot(χ, χ) * k^2))
+    @debug n χ.vals
+
+    # renormalizing χ
+    for i in eachindex(χ.vals)
+        χ.vals[i] *= n
+    end
+    return χ
+end
+
+PermutationGroups.conjugacy_classes(χ::Character) = χ.cc
 
 Base.@propagate_inbounds function Base.getindex(χ::Character, i::Integer)
     @boundscheck checkbounds(χ.vals, abs(i))
