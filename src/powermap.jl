d1d9f4132e776ea030fa6de7d574da4f8b5a8588
kalmarek <kalmar@amu.edu.pl>
2020-08-09 14:24:00 +0200

    Make PowerMap cyclic wrt powers

--- src/powermap.jl
+++ src/powermap.jl
@@ -1,4 +1,4 @@
-struct PowerMap{T} <: AbstractMatrix{Int}
+struct PowerMap{T<:AbstractOrbit} <: AbstractMatrix{Int}
     cc::Vector{T}
     cache::Matrix{Int}
 
@@ -6,7 +6,10 @@ struct PowerMap{T} <: AbstractMatrix{Int}
         ccG::AbstractVector{T},
         exp = exponent(ccG),
     ) where {T<:AbstractOrbit}
-        return new{T}(ccG, zeros(Int, length(ccG), exp))
+        cache = zeros(Int, length(ccG), exp)
+        cache[:, 1] .= 1
+        cache[:, 2] .= 1:length(ccG)
+        return new{T}(ccG, cache)
     end
 end
 
@@ -14,7 +17,9 @@ Base.axes(pm::PowerMap) = (Base.OneTo(length(pm.cc)), 0:size(pm.cache, 2)-1)
 Base.size(pm::PowerMap) = size(pm.cache)
 
 function Base.getindex(p::PowerMap, i::Integer, j::Integer)
-    # @boundscheck
+    j = mod(j, size(p, 2))
+    @boundscheck 1 ≤ i ≤ size(p, 1) || throw(Bounds)
+
     if iszero(p.cache[i, j+1])
         gj = first(p.cc[i])^j
         p.cache[i, j+1] = findfirst(cc -> gj ∈ cc, p.cc)
