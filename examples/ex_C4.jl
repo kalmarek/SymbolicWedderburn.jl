94a7d5bdfbd1885f60cfcfa274feb6253f1d130c
kalmarek <kalmar@amu.edu.pl>
2020-10-23 19:03:46 +0200

    update C4 example

--- examples/ex_C4.jl
+++ examples/ex_C4.jl
@@ -16,7 +16,6 @@ let m = SOSModel(SCS.Optimizer)
     optimize!(m)
 end
 
-
 # @constraint m  sum(x.^2) - t in SOSCone() symmetry_group = C2
 using DynamicPolynomials
 using MultivariatePolynomials
@@ -39,9 +38,10 @@ end
 
 include(joinpath(@__DIR__, "..", "test", "smallgroups.jl"));
 G = SmallPermGroups[4][1]
+# G = PermGroup([perm"(1,2)", perm"(1,2,3,4)"])
 chars_vars = SymbolicWedderburn.characters_dixon(G)
 
-mvec = reverse(monomials(x, 0:1))
+mvec = monomials(x, 0:1)
 
 chars_mvec = let chars = chars_vars, basis = mvec
 
@@ -60,26 +60,41 @@ chars_mvec = let chars = chars_vars, basis = mvec
     [SymbolicWedderburn.Character(values(χ), χ.inv_of, ccG_large) for χ in chars]
 end
 
-vr_chars = SymbolicWedderburn.real_vchars(chars_large)
-U = [SymbolicWedderburn.matrix_projection(χ) for χ in vr_chars]
+vr_chars = SymbolicWedderburn.real_vchars(chars_mvec)
+
+U = filter!(x->all(!iszero, x), [SymbolicWedderburn.matrix_projection(χ) for χ in vr_chars])
 
 R = map(U) do c_u
     u = last(c_u)
     if all(isreal, u)
-        SymbolicWedderburn.row_echelon_form(float.(u))
+        image_coeffs, pivots = SymbolicWedderburn.row_echelon_form(float.(u))
+        dim = length(pivots)
+        image_coeffs[1:dim, :]
     else
         throw("Not Implemented")
     end
 end
 
+R = filter!(!iszero ∘ first ∘ size, R)
+
 msym = let msym = SOSModel(SCS.Optimizer)
     @variable   msym t
     @objective  msym Max t
-    @variable   msym sos1 SOSPoly(FixedPolynomialBasis(first(R[1])[1:length(last(R[1])),:]*mvec))
-    @variable   msym sos2 SOSPoly(FixedPolynomialBasis(first(R[2])[1:length(last(R[2])),:]*mvec))
-    @variable   msym sos3 SOSPoly(FixedPolynomialBasis(first(R[3])[1:length(last(R[3])),:]*mvec))
-    @variable   msym sos4 SOSPoly(FixedPolynomialBasis(first(R[4])[1:length(last(R[4])),:]*mvec))
-    @constraint msym sum(x) + sum(x.^2) - t == sos1 + sos2 + sos3 + sos4
+
+    @variable   msym sos1 SOSPoly(FixedPolynomialBasis(R[1]*mvec))
+    @variable   msym sos2 SOSPoly(FixedPolynomialBasis(R[2]*mvec))
+    @variable   msym sos3 SOSPoly(FixedPolynomialBasis(R[3]*mvec))
+    # @variable   msym sos4
+    @constraint msym sum(x) + sum(x.^2) - t == sos1 + sos2 + sos3
     optimize!(msym)
     msym
 end
+
+termination_status(msym)
+
+value.(msym[:sos1].Q)
+
+value.(msym[:sos2].Q)
+
+value.(msym[:sos3].Q)
+
