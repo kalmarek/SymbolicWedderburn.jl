62da492b087e437510e560a76236a3bc2f8f84f4
kalmarek <kalmar@amu.edu.pl>
2020-12-21 02:59:58 +0100

    cleanup the examples

--- examples/ex_C4.jl
+++ examples/ex_C4.jl
@@ -12,75 +12,37 @@ let m = SOSModel(SCS.Optimizer)
     @variable m t
     @objective m Max t
     @variable m sos SOSPoly([1; x])
-    @constraint m sum(x) + sum(x.^2) - t == sos
+    @constraint m sum(x) + sum(x .^ 2) - t == sos
     optimize!(m)
 end
 
-# @constraint m  sum(x.^2) - t in SOSCone() symmetry_group = C2
-using DynamicPolynomials
-using MultivariatePolynomials
-const MP = MultivariatePolynomials
-using MultivariateBases
-
+# @constraint m  sum(x.^2) - t in SOSCone() symmetry_group = C4
 
 include("action_polynomials.jl")
 
+# include(joinpath(@__DIR__, "..", "test", "smallgroups.jl"));
+# G = SmallPermGroups[4][1]
 
-include(joinpath(@__DIR__, "..", "test", "smallgroups.jl"));
-G = SmallPermGroups[4][1]
+G = PermGroup([perm"(1,2,3,4)"])
 # G = PermGroup([perm"(1,2)", perm"(1,2,3,4)"])
-chars_vars = SymbolicWedderburn.characters_dixon(G)
 
-mvec = monomials(x, 0:1)
+basis = monomials(x, 0:1)
 
-chars_mvec = let chars = chars_vars, basis = mvec
+R = SymbolicWedderburn.symmetry_adapted_basis(G, basis)
 
-    @assert all(χ.inv_of == first(chars).inv_of for χ in chars)
+msym = let msym = SOSModel(SCS.Optimizer), basis = basis, R = R
+    @variable msym t
+    @objective msym Max t
 
-    induced_action = SymbolicWedderburn.OnPoints(basis)
-    ccG_large = induced_action.(conjugacy_classes(first(chars)))
+    @variable msym sos1 SOSPoly(FixedPolynomialBasis(R[1] * basis))
+    @variable msym sos2 SOSPoly(FixedPolynomialBasis(R[2] * basis))
+    @variable msym sos3 SOSPoly(FixedPolynomialBasis(R[3] * basis))
 
-    # double check:
-    let ccls = ccG_large, large_gens = induced_action.(gens(G))
-        G_large = PermGroup(large_gens)
-        ccG_large = conjugacy_classes(G_large)
-        @assert all(Set.(collect.(ccG_large)) .== Set.(collect.(ccls)))
-    end
-
-    [SymbolicWedderburn.Character(values(χ), χ.inv_of, ccG_large) for χ in chars]
-end
-
-vr_chars = SymbolicWedderburn.real_vchars(chars_mvec)
-
-U = filter!(x->all(!iszero, x), [SymbolicWedderburn.matrix_projection(χ) for χ in vr_chars])
-
-R = map(U) do c_u
-    u = last(c_u)
-    if all(isreal, u)
-        image_coeffs, pivots = SymbolicWedderburn.row_echelon_form(float.(u))
-        dim = length(pivots)
-        image_coeffs[1:dim, :]
-    else
-        throw("Not Implemented")
-    end
-end
-
-R = filter!(!iszero ∘ first ∘ size, R)
-
-msym = let msym = SOSModel(SCS.Optimizer)
-    @variable   msym t
-    @objective  msym Max t
-
-    @variable   msym sos1 SOSPoly(FixedPolynomialBasis(R[1]*mvec))
-    @variable   msym sos2 SOSPoly(FixedPolynomialBasis(R[2]*mvec))
-    @variable   msym sos3 SOSPoly(FixedPolynomialBasis(R[3]*mvec))
-    # @variable   msym sos4
-    @constraint msym sum(x) + sum(x.^2) - t == sos1 + sos2 + sos3
+    @constraint msym sum(x) + sum(x .^ 2) - t == sos1 + sos2 + sos3
     optimize!(msym)
+    @info termination_status(msym)
     msym
 end
-
-termination_status(msym)
 
 value.(msym[:sos1].Q)
 
--- examples/ex_motzkin.jl
+++ examples/ex_motzkin.jl
@@ -17,54 +17,19 @@ m = let m = SOSModel(optimizer_with_attributes(SCS.Optimizer, "eps"=>1e-5, "acce
     @variable m sos SOSPoly(monomials([x, y], 0:5))
     @constraint m motzkin - t == sos
     optimize!(m)
+    @info termination_status(m)
     m
 end
 
 include("action_polynomials.jl")
 
-
-include(joinpath(@__DIR__, "..", "test", "smallgroups.jl"));
 G = PermGroup([perm"(1,2)"])
-chars_vars = SymbolicWedderburn.characters_dixon(G)
+basis = monomials([x,y], 0:3)
 
-mvec = monomials([x,y], 0:3)
+R = SymbolicWedderburn.symmetry_adapted_basis(G, basis)
 
-chars_mvec = let chars = chars_vars, basis = mvec
-
-    @assert all(χ.inv_of == first(chars).inv_of for χ in chars)
-
-    induced_action = SymbolicWedderburn.OnPoints(basis)
-    ccG_large = induced_action.(conjugacy_classes(first(chars)))
-
-    # double check:
-    let ccls = ccG_large, large_gens = induced_action.(gens(G))
-        G_large = PermGroup(large_gens)
-        ccG_large = conjugacy_classes(G_large)
-        @assert all(Set.(collect.(ccG_large)) .== Set.(collect.(ccls)))
-    end
-
-    [SymbolicWedderburn.Character(values(χ), χ.inv_of, ccG_large) for χ in chars]
-end
-
-vr_chars = SymbolicWedderburn.real_vchars(chars_mvec)
-
-U = filter!(x->all(!iszero, x), [SymbolicWedderburn.matrix_projection(χ) for χ in vr_chars])
-
-R = map(U) do c_u
-    u = last(c_u)
-    if all(isreal, u)
-        image_coeffs, pivots = SymbolicWedderburn.row_echelon_form(float.(u))
-        dim = length(pivots)
-        image_coeffs[1:dim, :]
-    else
-        throw("Not Implemented")
-    end
-end
-
-R = filter!(!iszero ∘ first ∘ size, R)
-
-msym = let msym = SOSModel(optimizer_with_attributes(SCS.Optimizer,
-    "eps"=>1e-12,
+msym = let R=R, msym = SOSModel(optimizer_with_attributes(SCS.Optimizer,
+    "eps"=>3e-11,
     "max_iters"=>100_000,
     "acceleration_lookback"=>0,
     "alpha"=>1.95,
@@ -73,17 +38,14 @@ msym = let msym = SOSModel(optimizer_with_attributes(SCS.Optimizer,
     @variable   msym t
     @objective  msym Max t
 
-    @variable   msym sos1 SOSPoly(FixedPolynomialBasis(R[1]*mvec))
-    @variable   msym sos2 SOSPoly(FixedPolynomialBasis(R[2]*mvec))
+    @variable   msym sos1 SOSPoly(FixedPolynomialBasis(R[1]*basis))
+    @variable   msym sos2 SOSPoly(FixedPolynomialBasis(R[2]*basis))
 
     @variable   msym sos3 SOSPoly(monomials([x,y], 0:1))
 
     @constraint msym motzkin - t + 0.125 == sos1 + sos2 + (2 - x^2 - y^2)*sos3
     @constraint msym t <= -0.05
     optimize!(msym)
+    @info termination_status(msym)
     msym
 end
-
-
-termination_status(msym)
-
