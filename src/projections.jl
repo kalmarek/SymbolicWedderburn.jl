6d0df57e4fe2bbf9b364fee4bceb635d486fb91d
Marek Kaluba <kalmar@amu.edu.pl>
2021-04-29 00:57:09 +0200

    formatting

--- src/projections.jl
+++ src/projections.jl
@@ -18,8 +18,7 @@ end
 function matrix_projection(χ::AbstractClassFunction)
     deg = degree(χ)
     if iszero(deg) # short circuting the trivial case
-        return zeros(eltype(χ), 0, degree(first(first(conjugacy_classes(χ))))),
-        deg
+        return zeros(eltype(χ), 0, degree(first(first(conjugacy_classes(χ))))), deg
     end
     ordG = sum(length, conjugacy_classes(χ))
     U = matrix_projection(values(χ), conjugacy_classes(χ))
@@ -44,7 +43,7 @@ function matrix_projection(
 
     for (val, cc) in zip(vals, ccls)
         for g in cc
-            for i = 1:dim
+            for i in 1:dim
                 result[i, i^g] += val
             end
         end
@@ -72,9 +71,7 @@ end
     affordable_real!(chars::AbstractVector{<:AbstractClassFunction})
 Return _real_ characters formed from `chars` by replacing `χ` with `2re(χ)` when necessary.
 """
-function affordable_real!(
-    chars::AbstractVector{T},
-) where {T<:AbstractClassFunction}
+function affordable_real!(chars::AbstractVector{T}) where {T<:AbstractClassFunction}
     pmap = PowerMap(conjugacy_classes(first(chars)))
     res = affordable_real!.(chars, Ref(pmap))
     return unique!(res)
@@ -163,20 +160,19 @@ function symmetry_adapted_basis(
     chars::AbstractVector{<:AbstractClassFunction},
 ) where {T}
     ψ = action_character(conjugacy_classes(first(chars)))
+    real_chars = T <: Real ? affordable_real!(deepcopy(chars)) : chars
 
-    chars = T <: Real ? affordable_real!(deepcopy(chars)) : chars
+    multiplicities = Int[Int(dot(ψ, χ)) / Int(dot(χ, χ)) for χ in real_chars]
+    degrees = degree.(real_chars)
 
-    multiplicities = Int[dot(ψ, χ) / dot(χ, χ) for χ in chars]
-    degrees = PermutationGroups.degree.(chars)
-
-    @debug info "Decomposition into character spaces:
+    @debug "Decomposition into character spaces:
     degrees=$(join([lpad(d, 5) for d in degrees], ""))
     multips=$(join([lpad(m, 5) for m in multiplicities], ""))"
 
     dot(multiplicities, degrees) == degree(ψ) ||
         @warn "chars do not constitute a complete basis for action:
         $(dot(multiplicities, degrees)) ≠ $(degree(ψ))"
-    constituents = [χ for (χ, m) in zip(chars, multiplicities) if m ≠ 0]
+    constituents = [χ for (χ, m) in zip(real_chars, multiplicities) if m ≠ 0]
 
     return isotypical_basis.(constituents)
 end
