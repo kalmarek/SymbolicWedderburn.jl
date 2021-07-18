5dd1f807c52c2b60816fbf12f4263e692807d253
Marek Kaluba <kalmar@amu.edu.pl>
2021-07-18 19:26:29 +0200

    update examples to v0.1.0

--- examples/action_polynomials.jl
+++ examples/action_polynomials.jl
@@ -1,2 +1,10 @@
-permuted(exponent::AbstractVector, p::Perm) =
-    [exponent[i^p] for i in eachindex(exponent)]
+struct PermutingVariables <: SymbolicWedderburn.ByPermutations end
+
+function SymbolicWedderburn.action(::PermutingVariables, g::PermutationGroups.AbstractPerm, m::Monomial)
+    v = variables(m)
+    return m(v => map(i -> v[i^g], eachindex(v)))
+end
+
+function SymbolicWedderburn.action(::PermutingVariables, g::PermutationGroups.AbstractPerm, v::AbstractVector)
+    return map(i -> v[i^g], eachindex(v))
+end
