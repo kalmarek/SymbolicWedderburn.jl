73289dcd1d7073e8b2c41e0d274bbd69b0f7e3fd
Marek Kaluba <kalmar@amu.edu.pl>
2021-07-19 09:17:46 +0200

    run examples in the tests on julia 1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,3 +19,14 @@ include("characters.jl")
 include("projections.jl")
 include("action_permutation.jl")
 include("action_linear.jl")
+
+if VERSION >= v"1.6.0"
+    @testset "Examples" begin
+        using Pkg
+        Pkg.activate(joinpath(@__DIR__, "..", "examples"))
+        Pkg.instantiate()
+        include("../examples/ex_C2_linear.jl")
+        include("../examples/ex_C4.jl")
+        include("../examples/ex_motzkin.jl")
+    end
+end
