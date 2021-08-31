f7055bf2b9048bfb13349b75b76cef3911815d9f
Marek Kaluba <kalmar@amu.edu.pl>
2021-08-31 17:50:53 +0200

    rework symmetry_adapted_basis for the thrid time

--- src/sa_basis.jl
+++ src/sa_basis.jl
@@ -65,130 +65,194 @@ function affordable_real(
 end
 
 """
-    symmetry_adapted_basis([T::Type,] G::AbstractPermutationGroup[, S=Rational{Int}])
-Compute a basis for the linear space `ℝⁿ` which is invariant under the symmetry of `G`.
+    symmetry_adapted_basis([T::Type,] G::AbstractPermutationGroup[, S=Rational{Int};
+        semisimple=false])
+Compute a basis for the linear space `ℝⁿ` (where `n = degree(G)`) which is
+invariant under the symmetry of `G`.
 
-The permutation group is acting naturally on `1:degree(G)`. The coefficients of
-the invariant basis are returned in (orthogonal) blocks corresponding to irreducible
-characters of `G`.
+The coefficients of the invariant basis are returned in (orthogonal) blocks
+corresponding to irreducible characters of `G`.
 
 Arguments:
 * `S` controls the types of `Cyclotomic`s used in the computation of
 character table. Exact type are preferred. For larger groups `G` `Rational{BigInt}`
 might be necessary.
 * `T` controls the type of coefficients of the returned basis.
+* `semisimple`: if set to `false` (the default) an effort to find minimal
+projection system is made, so that the blocks give isomorphism to simple
+summands. Otherwise semisimple decomposition is computed.
 
 !!! Note:
-Each block is invariant under the action of `G`, i.e. the action may permute
-vectors from symmetry adapted basis within each block. The blocks are guaranteed
-to be orthogonal. If `T<:LinearAlgebra.BlasFloat` BLAS routines will be used to
-orthogonalize vectors within each block.
+Each returned block (a `DirectSummand`) is invariant under the action of `G`,
+which means that the action may still e.g. permute (row) vectors , but only
+*within* each block. The blocks are guaranteed to be orthogonal.
+If `T<:LinearAlgebra.BlasFloat` BLAS routines will be used to orthogonalize
+vectors within each block.
 """
-symmetry_adapted_basis(G::AbstractPermutationGroup, S::Type = Rational{Int}) =
-    _symmetry_adapted_basis(characters_dixon(S, G))
+function symmetry_adapted_basis(
+    G::AbstractPermutationGroup,
+    S::Type = Rational{Int};
+    semisimple::Bool = false,
+)
+    tbl = CharacterTable(S, G)
+    return symmetry_adapted_basis(eltype(tbl), tbl, semisimple = semisimple)
+end
 
-symmetry_adapted_basis(T::Type, G::AbstractPermutationGroup, S::Type = Rational{Int}) =
-    symmetry_adapted_basis(T, characters_dixon(S, G))
+symmetry_adapted_basis(
+    T::Type,
+    G::AbstractPermutationGroup,
+    S::Type = Rational{Int};
+    semisimple::Bool = false,
+) = symmetry_adapted_basis(T, CharacterTable(S, G), semisimple = semisimple)
+
+function symmetry_adapted_basis(
+    T::Type,
+    tbl::CharacterTable;
+    semisimple::Bool = false,
+)
+    irr, multips = _constituents_decomposition(
+        action_character(conjugacy_classes(tbl), tbl),
+        tbl,
+    )
+
+    if T <: Real
+        irr, multips = affordable_real(irr, multips)
+    end
+
+    if semisimple || all(isone ∘ degree, irr)
+        return _symmetry_adapted_basis(T, irr, multips)
+    else
+        RG = _group_algebra(parent(tbl))
+        return _symmetry_adapted_basis(T, irr, multips, RG)
+    end
+end
 
 """
-    symmetry_adapted_basis([T::Type,] G::Group, basis, action[, S=Rational{Int}])
-Compute a basis for the linear space spanned by `basis` which is invariant under
-the symmetry of `G`.
+    symmetry_adapted_basis([T::Type,] G::Group, action, basis[, S=Rational{Int}];
+        semisimple=false)
+Compute a decomposition of basis into (semi)simple subspaces which are invariant under
+the action of `G`.
 
-* The action used in these computations is
-> `(b,g) → action(b,g)` for `b ∈ basis`, `g ∈ G`
-and needs to be defined by the user.
-* It is assumed that `G` acts on a subset of basis and the action needs to be
+It is assumed that `G` acts on a subset of basis and the action needs to be
 extended to the whole `basis`. If `G` is a permutation group already acting on
 the whole `basis`, a call to `symmetry_adapted_basis(G)` is preferred.
 * For inducing the action `basis` needs to be indexable and iterable
 (e.g. in the form of an `AbstractVector`).
-
-Arguments:
-* `S` controlls the types of `Cyclotomic`s used in the computation of
-character table. Exact type are preferred. For larger groups `G` `Rational{BigInt}`
-might be necessary.
-* `T` controls the type of coefficients of the returned basis.
-
-!!! Note:
-Each block is invariant under the action of `G`, i.e. the action may permute
-vectors from symmetry adapted basis within each block. The blocks are guaranteed
-to be orthogonal. If `T<:LinearAlgebra.BlasFloat` BLAS routines will be used to
-orthogonalize vectors within each block.
 """
-function symmetry_adapted_basis(G::Group, basis, action, S::Type = Rational{Int})
-    chars_ext = extended_characters(S, G, basis, action)
-    return symmetry_adapted_basis(chars_ext)
+function symmetry_adapted_basis(
+    G::Group,
+    action::Action,
+    basis,
+    S::Type = Rational{Int};
+    semisimple=false,
+)
+    tbl = CharacterTable(S, G)
+    return symmetry_adapted_basis(eltype(tbl), tbl, action, basis, semisimple=semisimple)
 end
 
 function symmetry_adapted_basis(
-    ::Type{T},
+    T::Type,
     G::Group,
+    action::Action,
     basis,
-    action,
-    ::Type{S} = Rational{Int},
-) where {T,S}
-    chars_ext = extended_characters(S, G, basis, action)
-    return symmetry_adapted_basis(T, chars_ext)
-end
-
-symmetry_adapted_basis(chars::AbstractVector{<:AbstractClassFunction{T}}) where T =
-    symmetry_adapted_basis(T, chars)
-
-symmetry_adapted_basis(::Type{T}, chars::AbstractVector{<:AbstractClassFunction}) where T =
-    _symmetry_adapted_basis(Character{T}.(chars))
-
-symmetry_adapted_basis(T::Type{<:Real}, chars::AbstractVector{<:AbstractClassFunction}) =
-    _symmetry_adapted_basis(Character{T}.(affordable_real(chars)))
-
-symmetry_adapted_basis(::Type{T}, chars::AbstractVector{<:AbstractClassFunction{T}}) where T =
-    _symmetry_adapted_basis(chars)
-
-
-_multiplicities(ψ, chars) = Int[div(Int(dot(ψ, χ)), Int(dot(χ, χ))) for χ in chars]
-_multiplicities(ψ, chars::AbstractVector{<:AbstractClassFunction{T}}) where T<:AbstractFloat =
-    [round(Int, dot(ψ, χ) / dot(χ, χ)) for χ in chars]
-_multiplicities(ψ, chars::AbstractVector{<:AbstractClassFunction{T}}) where T<:Complex =
-    [round(Int, real(dot(ψ, χ) / dot(χ, χ))) for χ in chars]
-
-macro spawn_compat(expr)
-    @static if VERSION < v"1.3.0"
-        return :(@async $(esc(expr)))
-    else
-        return :(Threads.@spawn $(esc(expr)))
-    end
-end
-
-function symmetry_adapted_basis2(T::Type, G::Group, S::Type{<:Rational} = Rational{Int}, simple=true)
+    S::Type = Rational{Int};
+    semisimple=false,
+)
     tbl = CharacterTable(S, G)
-    ψ = action_character(conjugacy_classes(tbl), tbl)
+    return symmetry_adapted_basis(T, tbl, action, basis, semisimple=semisimple)
+end
 
-    irr_chars = irreducible_characters(tbl)
-    let multiplicities = constituents(ψ), degrees = degree.(irr_chars)
+function symmetry_adapted_basis(
+    T::Type,
+    tbl::CharacterTable,
+    action::Action,
+    basis;
+    semisimple=false,
+)
+    ehom = CachedExtensionHomomorphism(parent(tbl), action, basis, precompute=true)
+    ψ = action_character(ehom, tbl)
 
-        @debug "Decomposition into character spaces:
-        degrees:        $(join([lpad(d, 6) for d in degrees], ""))
-        multiplicities: $(join([lpad(m, 6) for m in multiplicities], ""))"
-
-        dot(multiplicities, degrees) == degree(ψ) ||
-            @error "Something went wrong: characters do not constitute a complete basis for action:
-            $(dot(multiplicities, degrees)) ≠ $(degree(ψ))"
+    irr, multips = _constituents_decomposition(ψ, tbl)
+    if T <: Real
+        irr, multips = affordable_real(irr, multips)
     end
 
-    RG = let G = parent(tbl)
-        b = StarAlgebras.Basis{UInt16}(vec(collect(G)))
-        StarAlgebra(G, b, (length(b), length(b)))
+    if semisimple || all(isone ∘ degree, irr)
+        return _symmetry_adapted_basis(T, irr, multips, ehom)
+    else
+        RG = _group_algebra(parent(tbl))
+        return _symmetry_adapted_basis(T, irr, multips, RG, ehom)
     end
+end
 
-    existing_chars = [i for (i, m) in enumerate(constituents(ψ)) if m ≠ 0]
-    multiplicities = constituents(ψ)[existing_chars]
+function _constituents_decomposition(ψ::Character, tbl::CharacterTable)
+    irr = irreducible_characters(tbl)
+    degrees = degree.(irr)
+    multiplicities = constituents(ψ)
 
-    mps, simple = minimal_projection_system(irr_chars[existing_chars], small_idempotents(RG))
+    @debug "Decomposition into character spaces:
+    degrees:        $(join([lpad(d, 6) for d in degrees], ""))
+    multiplicities: $(join([lpad(m, 6) for m in multiplicities], ""))"
 
-    res = map(zip(mps, multiplicities, simple)) do (µ, m, s)
-        µT = AlgebraElement{T}(µ)
-        @spawn_compat SemisimpleSummand(simple_basis(µT), m, s)
+    @assert dot(multiplicities, degrees) == degree(ψ)
+    "Something went wrong: characters do not constitute a complete basis for action:
+    $(dot(multiplicities, degrees)) ≠ $(degree(ψ))"
+
+    present_irreps = [i for (i, m) in enumerate(multiplicities) if m ≠ 0]
+    return irr[present_irreps], multiplicities[present_irreps]
+end
+
+function _group_algebra(G::Group)
+    @assert isfinite(G)
+    b = StarAlgebras.Basis{UInt16}(vec(collect(G)))
+    RG = if order(Int, G) <= (typemax(UInt16)>>2)
+        StarAlgebra(G, b, (length(b), length(b)), precompute=true)
+        # cache is about ~ 1Gb
+    else
+        StarAlgebra(G, b)
     end
+    return RG
+end
 
+function _symmetry_adapted_basis(
+    T::Type,
+    irr::AbstractVector{<:Character},
+    multiplicities::AbstractVector{<:Integer},
+    hom=nothing
+)
+    res = map(zip(irr, multiplicities)) do (µ, m)
+        @spawn_compat begin
+            µT = eltype(µ) == T ? µ : Character{T}(µ)
+            image = isnothing(hom) ? image_basis(µT) : image_basis(hom, µT)
+            simple = size(image, 1) == m
+            deg = degree(µ)
+            if deg == 1
+                @assert simple "Central projection associated to character is not simple unless its degree == 1"
+            end
+            DirectSummand(image, m, deg, simple)
+        end
+    end
+    return fetch.(res)
+end
+
+function _symmetry_adapted_basis(
+    T::Type,
+    irr::AbstractVector{<:Character},
+    multiplicities::AbstractVector{<:Integer},
+    RG::StarAlgebra{<:Group},
+    hom=nothing,
+)
+    mps, simples = minimal_projection_system(irr, RG)
+    degrees = degree.(irr)
+    res = map(zip(mps, multiplicities, degrees, simples)) do (µ, m, deg, simple)
+        @spawn_compat begin
+            µT = eltype(µ) == T ? µ : AlgebraElement{T}(µ)
+            image = isnothing(hom) ? image_basis(µT) : image_basis(hom, µT)
+            if simple
+                @assert size(image, 1) == m "The dimension of the projection doesn't match with simple summand multiplicity"
+            end
+            DirectSummand(image, m, deg, simple)
+        end
+    end
     return fetch.(res)
 end
