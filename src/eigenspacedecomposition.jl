73e2a27bfd0fe4f779df87e2117c3d007c7f0efa
Marek Kaluba <kalmar@amu.edu.pl>
2021-09-02 13:10:12 +0200

    add rref over AbstractFloats
    and remove rref over Cyclotomics{<:AbstractFloat}: this should be never
    called. Cyclotomics are intended for exact computations

--- src/eigenspacedecomposition.jl
+++ src/eigenspacedecomposition.jl
@@ -36,53 +36,56 @@ function row_echelon_form!(A::AbstractMatrix{T}) where {T<:Union{FiniteFields.GF
     return A, pivots
 end
 
-function row_echelon_form!(A::AbstractMatrix{C}) where {T<:AbstractFloat, C<:Cyclotomic{T}}
+function _findmax(f, A::AbstractArray)
+    mval, midx = f(first(A)), firstindex(A)
+    for idx in eachindex(A)
+        if (v = f(A[idx])) > mval
+            mval, midx = v, idx
+        end
+    end
+    return mval, midx
+end
+
+function row_echelon_form!(A::AbstractMatrix{T}, atol=eps(T)*max(size(A)...)) where {T<:AbstractFloat}
     pivots = Int[]
     pos = 0
     for i = 1:size(A, 2)
-
-        j = findnext(x -> abs(x) > sqrt(eps(T)), @view(A[:, i]), pos+1)
-        j === nothing && continue
+        # find the largest entry in the column below the last pivot
+        mval, midx = _findmax(abs, @view(A[pos+1:end, i]))
+        # j = findnext(x -> !iszero(x), , pos+1)
+        if mval < atol # the largest entry is below threshold so we zero everything in the column
+            A[pos+1:end, i] .= zero(T)
+            continue
+        end
+        j = midx+pos
         pos += 1
         push!(pivots, i)
 
+        # swap the rows so that
         if (pos != j)
             for k in 1:size(A, 2)
                 A[pos, k], A[j, k] = A[j, k], A[pos, k]
             end
         end
+        # A[pos, :] is the row with leading nz in i-th column
 
-        @assert abs(A[pos, i]) >= sqrt(eps(T))
         w = inv(A[pos, i])
-
+        # all columns to the left of i are zeroed (below pivots) so we start at i
         for colidx in i:size(A, 2)
-            if abs(A[pos, colidx]) <= sqrt(eps(T))
-                Cyclotomics.zero!(A[pos, colidx])
-            elseif colidx == i
-                Cyclotomics.one!(A[pos, colidx])
-            else
-                A[pos, colidx] *= w
-            end
+            A[pos, colidx] *= w
         end
+        # A[pos, i] is 1 now
 
+        # zero the whole i-th column above and below pivot:
         for rowidx = 1:size(A, 1)
-            rowidx == pos && continue
+            rowidx == pos && continue # don't zero the active row
             v = A[rowidx, i]
-            if abs(v) < sqrt(eps(T))
-                Cyclotomics.zero!(v)
-            else
-                for k in i:size(A, 2)
-                    A[rowidx, k] -= v * A[pos, k]
-                end
+            iszero(v) && continue
+            for k in i:size(A, 2) # to the left of pivot everything is zero
+                A[rowidx, k] -= v * A[pos, k]
             end
         end
     end
-
-    for i in eachindex(A)
-        ndigs = floor(Int, -log10(eps(T)) - log10(max(size(A)...)) - 3)
-        A[i] = Cyclotomics.roundcoeffs!(A[i], digits=ndigs)
-    end
-
     return A, pivots
 end
 
