102ba6fc97c4a1aba38e58ffcdeaaec1bc23e5ca
Marek Kaluba <kalmar@amu.edu.pl>
2024-04-18 10:37:06 +0200

    unify implementations of free words

--- test/action_permutation.jl
+++ test/action_permutation.jl
@@ -1,24 +1,4 @@
-struct Word{T}
-    alphabet::Vector{T}
-    letters::Vector{Int}
-
-    function Word(a::AbstractVector{T}, l::AbstractVector{<:Integer}) where {T}
-        all(i -> 1 <= i <= length(a), l) ||
-            throw(ArgumentError("Invalid word over alphabet $a: $w"))
-        return new{T}(a, l)
-    end
-end
-Base.show(io::IO, w::Word) = join(io, w.alphabet[w.letters], "·")
-
-function Base.:(==)(w::Word, v::Word)
-    return w.alphabet == v.alphabet && w.letters == v.letters
-end
-function Base.hash(w::Word, h::UInt = UInt(0))
-    return hash(w.alphabet, hash(w.letters, hash(Word, h)))
-end
-
-Base.:*(w::Word, v::Word) = Word(w.alphabet, [w.letters; v.letters])
-StarAlgebras.star(w::Word) = Word(w.alphabet, reverse(w.letters))
+include("free_words.jl")
 
 struct OnLetters <: SymbolicWedderburn.ByPermutations end
 function SymbolicWedderburn.action(
@@ -38,20 +18,6 @@ function SymbolicWedderburn.action(
     return (Word(w.alphabet, [w.letters[i]^p for i in eachindex(w.letters)]), 1)
 end
 
-function allwords(A, radius)
-    words = [Word(A, [i]) for i in 1:length(A)]
-    for r in 2:radius
-        append!(
-            words,
-            [
-                Word(A, collect(w)) for
-                w in Iterators.product(fill(1:length(A), r)...)
-            ],
-        )
-    end
-    return words
-end
-
 @testset "Extending homomorphism" begin
     fb_words = let A = [:a, :b, :c], radius = 4
         w = Word(A, [1, 2, 3, 2, 1])
@@ -61,7 +27,7 @@ end
               Word(A, [1, 3, 2, 3, 1])
 
         StarAlgebras.FixedBasis(
-            allwords(A, radius),
+            allwords(FreeWords(A), radius),
             StarAlgebras.DiracMStructure(*),
         )
     end
